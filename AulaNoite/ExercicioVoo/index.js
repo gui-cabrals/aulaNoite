d4507baa5084c77edb1c223cc9064b97547d9df0
Guilherme Cabral <103511015+gui-cabrals@users.noreply.github.com>
2023-09-14 21:44:10 -0300

    Add files via upload

---
+++ AulaNoite/ExercicioVoo/index.js
@@ -0,0 +1,20 @@
+//Cadastro de Voos
+
+let cadastroVoo = [{ 
+    numVoo: 0,
+    origem: 0,
+    destino: 0,
+    data: 0,
+    horarioPartida: 0,
+    assentos: 0,
+    precoAssento: 0,
+    
+}]
+
+let numeroVoo = 24
+
+if (cadastroVoo[0]) {
+    cadastroVoo[0].numVoo = numeroVoo
+}
+
+console.log(cadastroVoo)
